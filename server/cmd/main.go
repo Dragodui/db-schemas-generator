c8ad0da563ccf96567bccc44b67216d0d44f256b
Dragodui <alghazaliaks123@gmail.com>
2025-09-18 09:35:14 +0200

    feat: Add server

---
+++ server/cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/Dragodui/db-schemas-generator/internal/config"
+	"github.com/Dragodui/db-schemas-generator/internal/logger"
+	"github.com/go-chi/chi/v5"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// logger
+	logger.Init("app.log")
+
+	// config
+	cfg := config.Load()
+
+	// database
+	db, err := gorm.Open(postgres.Open(cfg.DB_DSN), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	// router with test route
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusAccepted)
+		json.NewEncoder(w).Encode("OK")
+	})
+
+	// serve server
+	if err := http.ListenAndServe(":"+cfg.Port, r); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ server/internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/lpernett/godotenv"
+)
+
+type Config struct {
+	DB_DSN    string
+	JWTSecret string
+	Port      string
+	ClientURL string
+}
+
+func Load() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(".env file is not exist or load incorrectly")
+	}
+
+	cfg := &Config{
+		Port:      os.Getenv("PORT"),
+		JWTSecret: os.Getenv("JWT_SECRET"),
+		DB_DSN:    os.Getenv("DB_DSN"),
+		ClientURL: os.Getenv("CLIENT_URL"),
+	}
+
+	if cfg.Port == "" {
+		cfg.Port = "8000"
+	}
+
+	if cfg.ClientURL == "" {
+		cfg.ClientURL = "https://localhost:5173"
+	}
+
+	return cfg
+}
